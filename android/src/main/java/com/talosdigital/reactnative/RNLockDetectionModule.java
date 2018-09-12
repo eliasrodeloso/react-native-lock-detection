a86f867c88a6dd8014ca06806824e45a72291749
Elías Rodelo <elias.rodelo@talosdigital.com>
2018-09-12 16:57:02 -0500

    Android code for detection when phone is locked

--- android/src/main/java/com/talosdigital/reactnative/RNLockDetectionModule.java
+++ android/src/main/java/com/talosdigital/reactnative/RNLockDetectionModule.java
@@ -1,13 +1,20 @@
 
 package com.talosdigital.reactnative;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
-import com.talosdigital.reactnative.ScreenReceiver;
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.app.KeyguardManager;
 
 public class RNLockDetectionModule extends ReactContextBaseJavaModule {
 
@@ -16,6 +23,7 @@ public class RNLockDetectionModule extends ReactContextBaseJavaModule {
   public RNLockDetectionModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
+    registerBroadcastReceiver();
   }
 
   @Override
@@ -23,18 +31,35 @@ public class RNLockDetectionModule extends ReactContextBaseJavaModule {
     return "RNLockDetection";
   }
 
-  /**
-   * PUBLIC REACT API
-   *
-   * isAvailable() Returns true if the fingerprint reader can be used
-   */
-  @ReactMethod
-  public void nowIsLocked(final Promise promise) {
-    try {
-      BroadcastReceiver myReceiver = new ScreenReceiver();
-      promise.resolve(myReceiver.onReceive());
-    } catch (Exception ex) {
-      promise.reject("ERR_UNEXPECTED_EXCEPTION", ex);
-    }
+  private void registerBroadcastReceiver() {
+    final IntentFilter theFilter = new IntentFilter();
+    /** System Defined Broadcast */
+    theFilter.addAction(Intent.ACTION_SCREEN_ON);
+    theFilter.addAction(Intent.ACTION_SCREEN_OFF);
+
+    BroadcastReceiver screenOnOffReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        String strAction = intent.getAction();
+
+        KeyguardManager myKM = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+
+        if (strAction.equals(Intent.ACTION_SCREEN_OFF) || strAction.equals(Intent.ACTION_SCREEN_ON)) {
+          if (myKM.inKeyguardRestrictedInputMode()) {
+            sendEvent("phoneLocked", "LOCKED");
+          } else {
+            sendEvent("phoneUnlocked", "UNLOCKED");
+          }
+        }
+      }
+    };
+    reactContext.registerReceiver(screenOnOffReceiver, theFilter);
   }
+
+  private void sendEvent(String eventName, String newPhoneStatus) {
+    WritableMap payload = Arguments.createMap();
+    payload.putString("newPhoneStatus", newPhoneStatus);
+    this.reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, payload);
+  }
+
 }
