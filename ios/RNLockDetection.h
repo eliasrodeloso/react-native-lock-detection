79f9493e024a2129c8c33d1003a36acf4470d246
Elías Rodelo <elias.rodelo@talosdigital.com>
2018-09-17 12:01:05 -0500

    iOS initial logic done

--- ios/RNLockDetection.h
+++ ios/RNLockDetection.h
@@ -1,11 +1,8 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
+#import <React/RCTEventEmitter.h>
 
-@interface RNLockDetection : NSObject <RCTBridgeModule>
+@interface RNLockDetection : RCTEventEmitter <RCTBridgeModule>
+
+- (void)lockStatusChanged:(NSString *)newStatus;
 
 @end
-  
--- ios/RNLockDetection.m
+++ ios/RNLockDetection.m
@@ -1,13 +1,65 @@
 
 #import "RNLockDetection.h"
+#import <React/RCTLog.h>
+
+RNLockDetection * refToSelf;
+
+//call back
+static void displayStatusChanged(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo)
+{
+    // the "com.apple.springboard.lockcomplete" notification will always come after the "com.apple.springboard.lockstate" notification
+
+    NSString *lockState = (__bridge NSString*)name;
+    RCTLogInfo(@"Darwin notification NAME = %@",name);
+
+    if([lockState isEqualToString:@"com.apple.springboard.lockcomplete"])
+    {
+        [refToSelf lockStatusChanged:@"LOCKED"];
+        RCTLogInfo(@"DEVICE LOCKED");
+    }
+    else
+    {
+        [refToSelf lockStatusChanged:@"NOT_LOCKED"];
+        RCTLogInfo(@"LOCK STATUS CHANGED");
+    }   
+}
+
 
 @implementation RNLockDetection
+
+- (id) init
+{
+    self = [super init];
+    refToSelf = self;
+    return self;
+}
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_MODULE(LockDetection);
+
+// Register the events for React Native
+- (NSArray<NSString *> *)supportedEvents
+{
+  return @[@"LockStatusChange"];
+}
+
+// function called to send the event to JavaScript
+- (void)lockStatusChanged:(NSString *)newStatus
+{
+  NSString *newLockedStatus = newStatus;
+  [self sendEventWithName:@"LockStatusChange" body:@{@"newStatus": newLockedStatus}];
+}
+
+RCT_EXPORT_METHOD(registerforDeviceLockNotif) {
+    RCTLogInfo(@"Registering for lock events");
+    //Screen lock notifications
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, displayStatusChanged, CFSTR("com.apple.springboard.lockcomplete"), NULL,CFNotificationSuspensionBehaviorDeliverImmediately);
+
+    CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, displayStatusChanged, CFSTR("com.apple.springboard.lockstate"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+}
 
 @end
-  
